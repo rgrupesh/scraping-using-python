bdba00be9078e2df60e13889a3252025d65980a0
rgrupesh <rupeshgelal9@gmail.com>
2020-09-23 16:02:51 +0545

    'login'

--- Selenium/scrap.py
+++ Selenium/scrap.py
@@ -1,12 +1,26 @@
 from selenium import webdriver
+import time
+
 
 PATH = r"C:\Program Files (x86)\chromedriver.exe"
 
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://rupeshgelal.com.np/")
+driver.get("https://www.linkedin.com/login?")
 
-print(driver.title)
+username = driver.find_element_by_id("username")
+username.send_keys("laxokir101@gridmauk.com")
+
+password = driver.find_element_by_id("password")
+password.send_keys("XXX")
+
+driver.find_element_by_xpath('//button[text()="Sign in"]').click()
+
+time.sleep(50)
 
 driver.quit()
+
+
+
+
 
