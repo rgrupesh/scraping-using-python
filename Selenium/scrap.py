dcc5b71d366024d500c1e361a34f41ed2a649eae
rgrupesh <rupeshgelal9@gmail.com>
2020-09-23 13:54:48 +0545

    'testing'

--- Selenium/scrap.py
+++ Selenium/scrap.py
@@ -1,1 +1,12 @@
 from selenium import webdriver
+
+PATH = r"C:\Program Files (x86)\chromedriver.exe"
+
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://rupeshgelal.com.np/")
+
+print(driver.title)
+
+driver.quit()
+
